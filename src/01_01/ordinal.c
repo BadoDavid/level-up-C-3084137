4a218a6ff4f1d287561f2cd4510b4a49e7ae2f45
Dávid Badó <bado.david@sch.bme.hu>
2023-05-15 12:31:38 +0000

    Adding Ctest

--- src/01_01/ordinal.c
+++ src/01_01/ordinal.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "ordinal.h"
 
 char *ordinal(int v)
 {
 	int tenth = v / 10;
-	int remainder = v % 10; 
+	int remainder = v % 10;
 	if (tenth != 1)
 	{
 		if (1 == remainder)
@@ -22,20 +24,28 @@ char *ordinal(int v)
 	return "th";
 }
 
+#ifndef USE_CTEST
 int main()
 {
 	int c;
 
-	for( c=1; c<=20; c++ )
+	for (c = 1; c <= 20; c++)
 	{
 		printf("%3d%s\t%3d%s\t%3d%s\t%3d%s\t%3d%s\n",
-				c, ordinal(c),
-				c+20, ordinal(c+20),
-				c+40, ordinal(c+40),
-				c+60, ordinal(c+60),
-				c+80, ordinal(c+80)
-			  );
+					 c, ordinal(c),
+					 c + 20, ordinal(c + 20),
+					 c + 40, ordinal(c + 40),
+					 c + 60, ordinal(c + 60),
+					 c + 80, ordinal(c + 80));
 	}
 
-	return(0);
+	return (0);
 }
+#else
+int main(int argc, char *argv[])
+{
+	const int inputValue = atoi(argv[1]);
+	printf("%3d%s\n", inputValue, ordinal(inputValue));
+	return (0);
+}
+#endif
